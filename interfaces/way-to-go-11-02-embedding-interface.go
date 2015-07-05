cfe770dc6c31c4cffb1d4302fe04fdfc74036297
Burin Choomnuan <agilecreativity@gmail.com>
2015-07-05 11:57:45 +1000

    Add more examples from The Way to Go

---
+++ interfaces/way-to-go-11-02-embedding-interface.go
@@ -0,0 +1,23 @@
+package main
+
+// Example of some type
+type Buffer string
+
+type ReadWrite interface {
+	Read(b Buffer) bool
+	Write(b Buffer) bool
+}
+
+type Lock interface {
+	Lock()
+	Unlock()
+}
+
+type File interface {
+	ReadWrite
+	Lock
+	Close()
+}
+
+func main() {
+}
---
+++ interfaces/way-to-go-11-03-type-classifier.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func classifier(items ...interface{}) {
+	for i, x := range items {
+		switch x.(type) {
+		case bool:
+			fmt.Printf("param #%d is a bool\n", i)
+		case float64:
+			fmt.Printf("param #%d is a float64\n", i)
+		case int, int64:
+			fmt.Printf("param #%d is an int\n", i)
+		case nil:
+			fmt.Printf("param #%d is nil\n", i)
+		case string:
+			fmt.Printf("param #%d is a string\n", i)
+		default:
+			fmt.Printf("param #%d's type is unknown\n", i)
+		}
+	}
+}
+
+func main() {
+	classifier(13, -14, 3, "Belgium", complex(1, 2), nil)
+}
