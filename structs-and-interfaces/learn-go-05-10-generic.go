e956308f2b3bf96dc181b168606d1b196f41e6d3
Burin Choomnuan <agilecreativity@gmail.com>
2015-07-15 20:03:56 +1000

    Add more examples

---
+++ structs-and-interfaces/learn-go-05-10-generic.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func Less(l, r interface{}) bool {
+	switch l.(type) {
+	case int:
+		if _, ok := r.(int); ok {
+			return l.(int) < r.(int)
+		}
+	case float32:
+		if _, ok := r.(int); ok {
+			return l.(float32) < r.(float32)
+		}
+	}
+	return false
+}
+
+func main() {
+	var a, b, c int = 5, 15, 0
+	var x, y, z float32 = 5.4, 29.3, 0.0
+	if c = a; Less(a, b) {
+		c = b
+	}
+
+	if z = x; Less(x, y) {
+		z = y
+	}
+
+	fmt.Println(c, z)
+}
+
+/* Output:
+15 5.4
+*/
