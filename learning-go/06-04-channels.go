146b524c35b787005c4832a06a8355f31abf507d
Burin Choomnuan <agilecreativity@gmail.com>
2015-07-20 08:05:35 +1000

    Add more examples

---
+++ learning-go/06-04-channels.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func shower(c chan int) {
+	for {
+		j := <-c
+		fmt.Printf("%d\n", j)
+	}
+}
+
+func main() {
+	ch := make(chan int)
+
+	go shower(ch)
+
+	for i := 0; i < 10; i++ {
+		ch <- i
+	}
+}
