feaf6f669ffb5e8523cfd4e1b7462635186a77b0
Burin Choomnuan <agilecreativity@gmail.com>
2015-07-20 19:11:16 +1000

    Add more examples

---
+++ learning-go/03-packages/even_client.go
@@ -0,0 +1,20 @@
+package main
+
+import "./even"
+import "fmt"
+
+func main() {
+	i, j := 5, 6
+
+	fmt.Printf("Is %d even? %v\n", i, even.Even(i))
+	fmt.Printf("Is %d odd?  %v\n", i, even.Odd(i))
+	fmt.Printf("Is %d even?  %v\n", j, even.Even(j))
+	fmt.Printf("Is %d odd?  %v\n", j, even.Odd(j))
+}
+
+/* Output:
+Is 5 even? false
+Is 5 odd?  true
+Is 6 even?  true
+Is 6 odd?  false
+*/
